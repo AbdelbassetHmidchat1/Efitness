995fafbce7b531fb36c5f2fe47278b18b7df6ad1
Abdelbasset <abdelbassethmidchat1@gmail.com>
2023-06-02 20:40:08 +0100

    changed link

--- client/config.js
+++ client/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = `https://efitness-hzdb31d77-abdelbassethmidchat1.vercel.app/api`;
+export const baseUrl = `https://efitness.vercel.app/api`;
 export const staticResourceUrl = `http://localhost:3000/uploads/`;
